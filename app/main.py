81a0032c880e5347474daeb8b494cdcef328e505
izeli4ka <108812531+izeli4ka@users.noreply.github.com>
2025-05-12 18:06:25 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -85,6 +85,6 @@ async def global_exception_handler(request: Request, exc: Exception):
         content={"detail": str(exc), "path": request.url.path}
     )
 
-if name == "main":
+if __name__ == "__main__":
     import uvicorn
     uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
